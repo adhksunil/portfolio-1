d747845101e58123bf93d6f264c7355cf14871a8
Sunil Adhikari <81942321+adhksunil@users.noreply.github.com>
2024-01-08 17:11:18 +0545

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from slimhamdi.net/tunis/dark/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 04 Sep 2023 15:25:48 GMT -->
+@include('layouts.header')
+<body>
+@yield('content')
+<!-- Template JS Files -->
+<script src="{{asset('js/jquery-3.5.0.min.js')}}"></script>
+<script src="{{asset('js/styleswitcher.js')}}"></script>
+<script src="{{asset('js/preloader.min.js')}}"></script>
+<script src="{{asset('js/fm.revealator.jquery.min.js')}}"></script>
+<script src="{{asset('js/imagesloaded.pkgd.min.js')}}"></script>
+<script src="{{asset('js/masonry.pkgd.min.js')}}"></script>
+<script src="{{asset('js/classie.js')}}"></script>
+<script src="{{asset('js/cbpGridGallery.js')}}"></script>
+<script src="{{asset('js/jquery.hoverdir.js')}}"></script>
+<script src="{{asset('js/popper.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
+</body>
+<!-- Mirrored from slimhamdi.net/tunis/dark/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 04 Sep 2023 15:26:02 GMT -->
+</html>
